c21035e04c1040dc235c8124ed866d6603252287
Kathleen Garrity <garrityk@bc.edu>
2020-01-21 14:00:49 -0500

    2.11 in Switft. App with buttons to change text & text color.

--- ButtonsAndStacks/ViewController.swift
+++ ButtonsAndStacks/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    var colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .systemIndigo, .systemPurple]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func colorButtonPressed(_ sender: UIButton) {
+        print("You clicked tag number " + String(sender.tag))
+        if sender.tag >= 0 && sender.tag <= 6 {
+            colorLabel.text = "You clicked \(sender.currentTitle!)"
+            colorLabel.textColor = colors[sender.tag]
+        } else {
+            colorLabel.text = ""
+        }
+    }
 }
 
